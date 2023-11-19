237c215d07fa6aa56d9f4b9af81c3d43b0b82455
Kazagashev <gimbarrabakan@gmail.com>
2023-11-19 12:46:24 +0700

    Fucj

---
+++ src/Le.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Le {
+    public static void main(String[] args) {
+
+        int [] varArray1 = {2, 3, 1, 5, 10};
+        int [] varArray2 = new int [varArray1.length];
+
+        for (int i = 0; i <varArray1.length; i ++) {
+
+            varArray2 [i] = varArray1 [i];
+            System.out.print(varArray2[i]);
+
+        }
+
+
+
+    }
+}
